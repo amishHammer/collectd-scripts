cb79a6a2694cc3abd61ad66a9686fb35898f5320
Tom Judge <tom@tomjudge.com>
2017-01-06 22:46:58 -0500

    Fix name

--- freebsd-smartmon-disk-temp.sh
+++ freebsd-smartmon-disk-temp.sh
@@ -6,6 +6,6 @@ SUDO=/usr/local/bin/sudo
 while sleep "$INTERVAL"; do
     for disk in `$SUDO /sbin/camcontrol devlist | /usr/bin/sed -E 's/^.*\\((.*),.*/\\1/'`; do
         temp=`$SUDO /usr/local/sbin/smartctl -a /dev/$disk  | grep Temperature_Celsius | awk '{ print $10; }'` 
-        echo "PUTVAL \"$HOSTNAME/disk-$disk/disk-temperature\" interval=$INTERVAL N:$temp"
+        echo "PUTVAL \"$HOSTNAME/disk-$disk/temperature\" interval=$INTERVAL N:$temp"
     done
 done
