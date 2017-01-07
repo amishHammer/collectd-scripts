1ddfb62ab430038dcd0c520c53b1e3bf0b327d14
Tom Judge <tom@tomjudge.com>
2017-01-06 22:01:20 -0500

    Add script to poll hard disk temps

---
+++ freebsd-smartmon-disk-temp.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+HOSTNAME="${COLLECTD_HOSTNAME:-localhost}"
+INTERVAL="${COLLECTD_INTERVAL:-60}"
+SUDO=/usr/local/bin/sudo
+while sleep "$INTERVAL"; do
+    for disk in `$SUDO /sbin/camcontrol devlist | /usr/bin/sed -E 's/^.*\\((.*),.*/\\1/'`; do
+        temp=`$SUDO /usr/local/sbin/smartctl -a /dev/$disk  | grep Temperature_Celsius | awk '{ print $10; }'` 
+        echo "PUTVAL \"$HOSTNAME/disk-$disk/disk-temperature\" interval=$INTERVAL N:$temp"
+    done
+done
